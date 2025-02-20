e7d67776d3e8326eb07812aad1f9008d4c631a1b
suding45 <suding3@nate.com>
2025-02-20 15:46:35 +0900

    0220

--- web1028_민준/70_pf/강원천문대/js/JS.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".navi > li").mouseover(function(){
-		$(".submenu, .menuBG").stop().slideDown();
-	});
-	$(".navi > li").mouseout(function(){	
-		$(".submenu, .menuBG").stop().slideUp();
-	});
-});
